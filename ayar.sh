86bfbacd2162f31be6ab679fd3600da957888ea5
yuceltoluyag <ytoluyag@gmail.com>
2018-03-31 00:24:48 +0300

    dil dosyaları problemi giderildi

--- ayar.sh
+++ ayar.sh
@@ -14,11 +14,24 @@ zaman() {
 
 dil() {
   echo -e "$red (1/2) >>>>> Dil Ayarları Yapılandırılıyor.   $reset"
-  echo -e "tr_TR.UTF-8" > /etc/locale.gen
+  echo -e "localectl set-locale LANG=tr_TR.UTF-8 UTF-8"
   locale-gen
 
   echo -e "$red (2/2) >>>>> Sistem dili yapılandırılıyor.                           $reset"
-  echo "LANG=tr_TR.UTF-8" > /etc/locale.conf
+  echo "LANG=tr_TR.UTF-8
+LC_CTYPE="tr_TR.UTF-8"
+LC_NUMERIC="tr_TR.UTF-8"
+LC_TIME="tr_TR.UTF-8"
+LC_COLLATE="tr_TR.UTF-8"
+LC_MONETARY="tr_TR.UTF-8"
+LC_MESSAGES=tr_TR.UTF-8
+LC_PAPER="tr_TR.UTF-8"
+LC_NAME="tr_TR.UTF-8"
+LC_ADDRESS="tr_TR.UTF-8"
+LC_TELEPHONE="tr_TR.UTF-8"
+LC_MEASUREMENT="tr_TR.UTF-8"
+LC_IDENTIFICATION="tr_TR.UTF-8"
+LC_ALL=" > /etc/locale.conf
 }
 
 makine() {
