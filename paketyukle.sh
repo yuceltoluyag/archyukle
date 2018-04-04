b2a4b8dde8824026e80b5f7ec4e18bb31a372b78
yücel <ytoluyag@gmail.com>
2018-04-04 03:14:45 +0300

    git

--- paketyukle.sh
+++ paketyukle.sh
@@ -8,7 +8,21 @@ packages+=( yaourt ) # madirfakir uykum geldi yatacam :D
 packages+=( cairo fontconfig freetype2 ttf-anonymous-pro ttf-dejavu ttf-liberation ttf-inconsolata ttf-ubuntu-font-family ttf-croscore ttf-droid ttf-roboto adobe-source-code-pro-fonts adobe-source-sans-pro-fonts adobe-source-serif-pro-fonts) # fontlar
 packages+=( xorg xorg-server xorg-xinit lxdm) # sistem dosyaları
 packages+=( a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins  ) # Kodekler
-packages+=( vlc gimp kdenlive ) # madirfakir uykum geldi yatacam :D
+packages+=( vlc gimp kdenlive git curl wget filezilla) # madirfakir uykum geldi yatacam :D
 packages+=( p7zip unrar file-roller wget gvfs networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc pulseaudio-alsa pavucontrol xfce4-pulseaudio-plugin  ) # madirfakir uykum geldi yatacam :D
 packages+=( i3 dmenu xorg xorg-xinit feh ) # masaüstü
 pacman --noconfirm --needed -S  ${packages[@]}
+
+# Git ayarları
+read -p "Github ayarlarınızı yapalım mı (e/h): " gitsec
+if [ "$gitsec" == "e" ] || [ "$gitsec" == "e" ]
+then
+  read -p "github adınız: " githubadi
+  read -p "github mailiniz: " githubmail
+fi
+# Github Yapılandırma
+if [ "$gitsec" == "e" ] || [ "$gitsec" == "e" ]
+then
+  git config --global user.name $githubadi
+  git config --global user.email $githubmail
+fi
